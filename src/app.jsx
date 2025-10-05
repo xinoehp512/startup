bbc4b4a07528ae1dbcc3f47a529b539961c17bc4
xinoehp512 <ryanmarkrichards2@gmail.com>
2025-10-04 23:02:06 -0600

    Added Routes

--- src/app.jsx
+++ src/app.jsx
@@ -5,11 +5,17 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Schedule } from './schedule/schedule';
+import { Login } from './login/login';
+import { AddTask } from './addtask/addtask';
+import { Social } from './social/social';
+import { Profile } from './profile/profile';
 
 
 export default function App() {
     return (
-        <>
+        <BrowserRouter>
             <header>
                 <Navbar expand="lg" className="bg-body-tertiary">
                     <Container>
@@ -17,25 +23,33 @@ export default function App() {
                         <Navbar.Toggle aria-controls="navbarSupportedContent" />
                         <Navbar.Collapse id="navbarSupportedContent">
                             <Nav className="navbar-nav me-auto">
-                                <Nav.Link href="schedule.html">My Schedule</Nav.Link>
-                                <Nav.Link href="login.html">Login</Nav.Link>
-                                <Nav.Link href="addtask.html">Add Task</Nav.Link>
-                                <Nav.Link href="social.html">Chat with Friends</Nav.Link>
-                                <Nav.Link href="profile.html">User Unknown</Nav.Link>
+                                <Nav.Link as={NavLink} className='nav-link' to="">My Schedule</Nav.Link>
+                                <Nav.Link as={NavLink} className='nav-link' to="login">Login</Nav.Link>
+                                <Nav.Link as={NavLink} className='nav-link' to="addtask">Add Task</Nav.Link>
+                                <Nav.Link as={NavLink} className='nav-link' to="social">Chat with Friends</Nav.Link>
+                                <Nav.Link as={NavLink} className='nav-link' to="profile">User Unknown</Nav.Link>
                             </Nav>
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
             </header>
 
-            <main>
-                App components go here.
-            </main>
+            <Routes>
+                <Route path='/' element={<Schedule />} exact />
+                <Route path='/login' element={< Login />} exact />
+                <Route path='/addtask' element={<AddTask />} exact />
+                <Route path='/social' element={<Social />} exact />
+                <Route path='/profile' element={<Profile />} exact />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
 
             <footer className="container-fluid bg-dark text-white-50">
                 <span>Created by Ryan Richards</span>
                 <a className="text-reset" href="https://github.com/xinoehp512/startup">Link to GitHub</a>
             </footer>
-        </>
+        </BrowserRouter>
     );
+}
+function NotFound() {
+    return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
