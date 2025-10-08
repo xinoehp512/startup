2a8c283dd3553c5585ee28e846cae7016fb65a6f
xinoehp512 <ryanmarkrichards2@gmail.com>
2025-10-08 13:20:28 -0600

    Converted Profile

--- src/profile/profile.jsx
+++ src/profile/profile.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './profile.css';
 
 export function Profile() {
     return (
-        <main className="container-fluid bg-secondary text-center">
-            <div>login displayed here</div>
+        <main>
+            <div class="profile-card">
+                <div class="profile-img"><img src="favicon.png" /></div>
+                <div class="profile-name">User Unknown</div>
+            </div>
         </main>
     );
 }
