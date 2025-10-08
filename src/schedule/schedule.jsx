4dc67424b7938c2c58cb87f2b8ef99148c0a19f4
xinoehp512 <ryanmarkrichards2@gmail.com>
2025-10-08 13:22:52 -0600

    Converted Schedule

--- src/schedule/schedule.jsx
+++ src/schedule/schedule.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './schedule.css';
 
 export function Schedule() {
     return (
-        <main className="container-fluid bg-secondary text-center">
-            <div>login displayed here</div>
+        <main>
+            <div class="schedule">
+                <div class="hours">
+                    <div class="hour">8:00 AM</div>
+                    <div class="hour">9:00 AM</div>
+                    <div class="hour">10:00 AM</div>
+                    <div class="hour">11:00 AM</div>
+                </div>
+                <div class="tasks">
+                    <div class="task-block">
+                        <div class="task">Brush Teeth</div>
+                        <div class="task">Eat Breakfast</div>
+                    </div>
+                    <div class="task-block">
+                        <div class="task">Task 1</div>
+                        <div class="task">Task 2</div>
+                    </div>
+                    <div class="task-block">
+                        <div class="task">Task 3</div>
+                        <div class="task">Task 4</div>
+                    </div>
+                    <div class="task-block">
+                        <div class="task">Meeting with boss for a very extremely terribly long time with a long description
+                        </div>
+                    </div>
+                </div>
+            </div>
         </main>
     );
 }
